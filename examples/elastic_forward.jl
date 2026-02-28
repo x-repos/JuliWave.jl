106ebc0e76e752e1dc9e41c3428e663361001101
ben <sipkaro@gmail.com>
2026-02-27 22:47:29 -0700

    x

--- examples/elastic_forward.jl
+++ examples/elastic_forward.jl
@@ -41,11 +41,53 @@ println("Time steps: $(nt), dt=$(round(dt*1e3, digits=3)) ms")
 println("Courant number: $(round(check_cfl(vp_val, dt, dx, dy), digits=3))")
 println()
 
-# Run simulation
+# Run simulation with wavefield snapshots
 println("Running elastic forward simulation...")
-seis_vx, seis_vy = simulate_elastic(model, geometry, config)
+seis_vx, seis_vy, snaps_vx, snaps_vy = simulate_elastic_wavefield(
+    model, geometry, config; save_every=25)
 
 println("Done!")
 println("Seismogram size: $(size(seis_vx))")
 println("Max |vx|: $(maximum(abs.(seis_vx)))")
 println("Max |vy|: $(maximum(abs.(seis_vy)))")
+println("Snapshots: $(size(snaps_vx, 3)) frames")
+
+# Save wavefield snapshots as PPM images
+outdir = joinpath(@__DIR__, "..", "output")
+mkpath(outdir)
+
+function save_ppm(filename, field; power=0.3)
+    ny_img, nx_img = size(field, 2), size(field, 1)
+    maxamp = maximum(abs.(field))
+    maxamp == 0.0 && (maxamp = 1.0)
+    open(filename, "w") do io
+        println(io, "P3")
+        println(io, "$nx_img $ny_img")
+        println(io, "255")
+        for jj in ny_img:-1:1
+            for ii in 1:nx_img
+                v = clamp(field[ii, jj] / maxamp, -1.0, 1.0)
+                if abs(v) < 0.01
+                    print(io, "255 255 255 ")
+                elseif v >= 0
+                    r = round(Int, 255 * v^power)
+                    print(io, "$r 0 0 ")
+                else
+                    b = round(Int, 255 * abs(v)^power)
+                    print(io, "0 0 $b ")
+                end
+            end
+            println(io)
+        end
+    end
+end
+
+for i in 1:size(snaps_vx, 3)
+    t_ms = round((i - 1) * 25 * dt * 1000, digits=1)
+    save_ppm(joinpath(outdir, "vx_$(lpad(i, 3, '0')).ppm"), snaps_vx[:, :, i])
+    save_ppm(joinpath(outdir, "vy_$(lpad(i, 3, '0')).ppm"), snaps_vy[:, :, i])
+    println("  Saved snapshot $i (t=$(t_ms) ms)")
+end
+
+println("\nSnapshots saved to: $outdir")
+println("View with: display $outdir/vx_*.ppm  (or use eog, feh, gimp)")
